fd6546b7978e0e1984f4fee8ead367932af21331
faizul <faizulcse@gmail.com>
2022-01-16 01:05:22 +0600

    update dependencies

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -8,8 +8,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src\\test\\java\\features"},
-        glue = "stepDefinitions",
-        tags = {"@test-02"}
+        glue = {"stepDefinitions"},
+        tags = "@test-02",
+        plugin = {"pretty", "html:target/cucumber-html", "json:target/cucumber.json"}
 )
 
 public class TestRunner {
